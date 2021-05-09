ed05541bc2d4cc3cf9c11f49681bbcda3b3373be
Sakar Aryal <techiesakar@gmail.com>
2021-05-10 00:09:15 +0545

    first commit

---
+++ assets/js/header.js
@@ -0,0 +1,36 @@
+function openSearch() {
+    document.getElementById("searchInput").focus();
+}
+
+function openSearch() {
+    var element = document.getElementById("myOverlay");
+    element.classList.add("active");
+}
+
+function closeSearch() {
+    var element = document.getElementById("myOverlay");
+    element.classList.remove("active");
+}
+
+function openMenu() {
+    document.getElementById("mobileMenu").style.right = "0";
+    document.getElementById("menuBtn").style.visibility = "hidden";
+
+}
+
+function closeMenu() {
+    document.getElementById("mobileMenu").style.right = "-100%";
+    document.getElementById("menuBtn").style.visibility = "visible";
+}
+var x = 0;
+
+function openSubMenu() {
+
+    if (x == 0) {
+        document.getElementById("mobileSubMenu").style.display = "block";
+        x = 1;
+    } else {
+        document.getElementById("mobileSubMenu").style.display = "none";
+        x = 0
+    }
+}
