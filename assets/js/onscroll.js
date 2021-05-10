ec4323001b35cf9e6499639873b6811ce90428b0
Sakar Aryal <techiesakar@gmail.com>
2021-05-10 22:02:47 +0545

    Scroll Top Working

--- assets/js/onscroll.js
+++ assets/js/onscroll.js
@@ -1,12 +1,12 @@
-//Get the button:
-mybutton = document.getElementById("myBtn");
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton.style.display = "flex";
   } else {
     mybutton.style.display = "none";
   }
@@ -14,6 +14,6 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-} 
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
